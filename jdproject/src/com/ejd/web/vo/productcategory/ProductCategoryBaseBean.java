e34905dc236205740a56aa460158e9497a10dabb
jqhero79 <jqhero79@gmail.com>
2009-03-15 09:00:35 +0000

    bug001

---
+++ jdproject/src/com/ejd/web/vo/productcategory/ProductCategoryBaseBean.java
@@ -0,0 +1,19 @@
+package com.ejd.web.vo.productcategory;
+
+import com.ejd.model.service.iface.IProductCategoryService;
+
+
+public class ProductCategoryBaseBean {
+	private IProductCategoryService productCategoryService;
+
+	public IProductCategoryService getProductCategoryService() {
+		return productCategoryService;
+	}
+
+	public void setProductCategoryService(
+			IProductCategoryService productCategoryService) {
+		this.productCategoryService = productCategoryService;
+	}
+	
+
+}
