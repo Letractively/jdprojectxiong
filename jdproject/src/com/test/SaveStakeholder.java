98b4531e5a408401534a567aa9209f4c9a7d082c
jqhero79 <jqhero79@gmail.com>
2009-04-13 10:02:54 +0000

    20090413

---
+++ jdproject/src/com/test/SaveStakeholder.java
@@ -0,0 +1,47 @@
+package com.test;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.jsf.FacesContextUtils;
+
+import com.ejd.web.bo.Address;
+import com.ejd.web.bo.Bank;
+import com.ejd.web.bo.Person;
+import com.ejd.web.bo.Stakeholder;
+public class SaveStakeholder extends HibernateDaoSupport {
+	SessionFactory at;
+ public SaveStakeholder(){
+	ApplicationContext appctx = FacesContextUtils.getWebApplicationContext(FacesContext.getCurrentInstance());
+
+	at=(SessionFactory)appctx.getBean("sessionFactory",SessionFactory.class);
+	Session aa = at.openSession();
+	 Stakeholder s = new Stakeholder("abcd", "bcde", "aaaaaaaaa", "sc", "yc", "ym",
+				"121212221", "1221212", "www.sohu.com", "woaini", "A", "C", "1342222222");
+	aa.beginTransaction();
+	aa.save(s);
+	aa.flush();
+ }
+ public static void save() {
+	 Stakeholder s = new Stakeholder("abcd", "bcde", "aaaaaaaaa", "sc", "yc", "ym",
+				"121212221", "1221212", "www.sohu.com", "woaini", "A", "C", "1342222222");
+	 
+ }
+ public static void main(String[] args) throws Exception {
+	 SaveStakeholder aa = new SaveStakeholder();
+ }
+public SessionFactory getAt() {
+	return at;
+}
+public void setAt(SessionFactory at) {
+	this.at = at;
+}
+
+ 
+}
