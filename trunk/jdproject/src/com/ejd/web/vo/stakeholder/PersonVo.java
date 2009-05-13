0699166b90827ab4cd6883979e95190569239e15
jqhero79@gmail.com <jqhero79@gmail.com@54522192-1092-11de-a058-67ab0bfb5968>
2009-05-13 16:14:08 +0000

    4554

--- trunk/jdproject/src/com/ejd/web/vo/stakeholder/PersonVo.java
+++ trunk/jdproject/src/com/ejd/web/vo/stakeholder/PersonVo.java
@@ -158,7 +158,6 @@ public class PersonVo implements Serializable {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + age;
 		return result;
 	}
 	public boolean equals(Object obj) {
