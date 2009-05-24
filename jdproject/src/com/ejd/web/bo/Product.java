9f54e7dc29691e96a6cdb1a017eb1c173f701e9e
jqhero79 <jqhero79@gmail.com>
2009-05-24 15:33:13 +0000

    bug20090523

---
+++ jdproject/src/com/ejd/web/bo/Product.java
@@ -0,0 +1,34 @@
+package com.ejd.web.bo;
+
+/**
+ * Product entity.
+ * 
+ * @author MyEclipse Persistence Tools
+ */
+public class Product extends AbstractProduct implements java.io.Serializable {
+
+	// Constructors
+
+	/** default constructor */
+	public Product() {
+	}
+
+	/** minimal constructor */
+	public Product(String code, Stakeholder provider, Productcategory primaryCategory,
+			Productcategory secondCategory, Productbrand brand, String status) {
+		super(code, provider, primaryCategory, secondCategory, brand,
+				status);
+	}
+
+	/** full constructor */
+	public Product(String code, String manufacturerCode, String name,
+			Stakeholder provider, Productcategory primaryCategory,
+			Productcategory secondCategory, Productbrand brand, String status,
+			Double purchaseprice, Double tradePriceOne, Double tradePriceTwo,
+			String retailPrice, String introduceFileName) {
+		super(code, manufacturerCode, name, provider, primaryCategory,
+				secondCategory, brand, status, purchaseprice,
+				tradePriceOne, tradePriceTwo, retailPrice, introduceFileName);
+	}
+
+}
