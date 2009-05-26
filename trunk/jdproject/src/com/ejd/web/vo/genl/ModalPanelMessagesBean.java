fcecce370805cd09418e49bed2f25dc37fd57374
jqhero79@gmail.com <jqhero79@gmail.com@54522192-1092-11de-a058-67ab0bfb5968>
2009-05-26 09:16:04 +0000

    Bug20090526

---
+++ trunk/jdproject/src/com/ejd/web/vo/genl/ModalPanelMessagesBean.java
@@ -0,0 +1,18 @@
+package com.ejd.web.vo.genl;
+
+public class ModalPanelMessagesBean {
+	private String messages;
+
+	public ModalPanelMessagesBean(){
+		
+	}
+	public String getMessages() {
+		return messages;
+	}
+
+	public void setMessages(String messages) {
+		this.messages = messages;
+	}
+	
+
+}
