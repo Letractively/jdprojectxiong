3f5fb9a760098e4307e02d9c1f80afd06f546829
jqhero79@gmail.com <jqhero79@gmail.com@54522192-1092-11de-a058-67ab0bfb5968>
2009-07-05 14:06:41 +0000

    20090708

--- trunk/jdproject/WebRoot/css/common.js
+++ trunk/jdproject/WebRoot/css/common.js
@@ -6,7 +6,7 @@ function changeFinishInCreateProduct(data){
    if (undefined == data || data.length <2) {
 	   //do nothing
    } else {
-   popup = window.open("./popup/popupStakeholder.jsf",
+   popup = window.open("http://localhost:8080/jdproject/popup/popupStakeholder.jsf",
            "popup", 
            "height=520,width=500,toolbar=no,menubar=no,"
            + "scrollbars=no");               
